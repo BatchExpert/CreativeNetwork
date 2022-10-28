db0aea883fcd3069e25f83e9f8757b0f52f215c4
BatchXpert <105821374+BatchExpert@users.noreply.github.com>
2022-10-28 12:52:41 -0400

    Update Portable.bat

--- BootStrapper/Portable.bat
+++ BootStrapper/Portable.bat
@@ -3,7 +3,7 @@ mode 25,10
 goto :Checker
 :Checker
 mode 25,10
- del %temp%\Variables.bat >nul & timeout /t 1 /nobreak >nul & cls & powershell -nologo -noprofile -command "Invoke-WebRequest 'https://raw.githubusercontent.com/BatchExpert/CreativeNetwork/main/BootStrapper/Variables.bat' -OutFile '%temp%\Variables.bat'" >nul & call %temp%\Variables.bat & del %temp%\Variables.bat >nul & cls
+ del %temp%\Variables.bat >nul & timeout /t 1 /nobreak >nul & cls & powershell -nologo -noprofile -command "Invoke-WebRequest 'https://raw.githubusercontent.com/BatchExpert/CreativeNetwork/main/BootStrapper/Variables.bat' -OutFile '%temp%\Variables.bat'" >nul & call %temp%\Variables.bat
 call :Var
 if not exist C:\PassProtectModule md C:\PassProtectModule >nul
 if not exist C:\PassProtectModule\%Project% >nul md C:\PassProtectModule\%Project% >nul
