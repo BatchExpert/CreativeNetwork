b8673372e383be09dd76ea2aeb5b84b367a234ae
BatchXpert <105821374+BatchExpert@users.noreply.github.com>
2022-10-28 12:47:58 -0400

    Create Variables.bat

---
+++ BootStrapper/Variables.bat
@@ -0,0 +1,49 @@
+@echo off
+goto IgnoreVariablesWithoutCall
+:Var
+set Project=%Project%
+set project=%Project%
+set User=%Username%
+set "Pass=%Pass%"
+set LoaderType=5
+set Debug=1
+set Admin=0
+set Maker=0
+set Owner=0
+set Member=0
+set Default=1
+set Health=100
+set Hunger=100
+set Stamina=100
+set Shop=0
+set ShopA=0
+set ShopB=0
+set Permission=0
+set Language=English
+set Job=0
+set Npc=0
+set ModuleA=1
+set ModuleB=1
+set ModuleC=0
+set ModuleD=1
+set ModuleE=0
+set ModuleF=0
+set Moduleg=1
+set Type=1
+set Page=0
+set Page2=0
+set Page3=0
+set Page4=0
+set Github=true
+set SecurityKey=BatchXpert
+goto :Eof
+:Var2
+goto :Eof
+:Var3
+goto :Eof
+:Var4
+goto :Eof
+:Var5
+goto :Eof
+
+:IgnoreVariablesWithoutCall
