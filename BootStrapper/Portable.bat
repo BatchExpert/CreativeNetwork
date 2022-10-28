a80db0959dbf73a5ec94fdc3087fb3f03fbf1a14
BatchXpert <105821374+BatchExpert@users.noreply.github.com>
2022-10-28 11:23:47 -0400

    Update Portable.bat

--- BootStrapper/Portable.bat
+++ BootStrapper/Portable.bat
@@ -378,6 +378,7 @@ if %t19%==blank set "t19="
 if %t20%==blank set "t20="
 set "Pass=%t1%%t2%%t3%%t4%%t5%%t6%%t7%%t8%%t9%%t10%%t11%%t12%%t13%%t14%%t15%%t16%%t17%%t18%%t19%%t20%"
 set BlurPass=%tD1%%tD2%%tD3%%tD4%%tD5%%tD6%%tD7%%tD8%%tD9%%tD10%%tD11%%tD12%%tD13%%tD14%%tD15%%tD16%%tD17%%tD18%%tD19%%tD20%
+:Redo
 mode 25,10
 cls
 title  
@@ -386,11 +387,14 @@ echo.
 echo Table: [A-Z,a-z,0-9]
 echo Password Length: %Length%
 echo.
-echo Insert Your Password:
+echo Insert Protection Key:
 echo %BlurPass%
 set /p "PW="
 if "%PW%"=="%Pass%" cls & goto StartUpRunUp
-goto DEC5
+cls
+echo Invalid Protection Key
+timeout /t 2 /nobreak >nul
+goto Redo
 ::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Paste Your Code Below The :PasteHere          ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::
