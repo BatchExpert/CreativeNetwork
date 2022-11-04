8ff15f7b83150c6d800c3de63b739d8e5191c1e4
BatchXpert <105821374+BatchExpert@users.noreply.github.com>
2022-11-04 13:12:52 -0400

    Update Injector.bat

--- BootStrapper/Injector.bat
+++ BootStrapper/Injector.bat
@@ -3,4 +3,4 @@ cls
 set Project=%~2
 set PWD=%~3
 set KEY=%~4
-del %temp%\Portable2.bat >nul & timeout /t 1 /nobreak >nul & cls & powershell -nologo -noprofile -command "Invoke-WebRequest 'https://raw.githubusercontent.com/BatchExpert/CreativeNetwork/main/BootStrapper/Portable.bat' -OutFile '%temp%\Portable2.bat'" >nul & call %temp%\Portable2.bat "%~1" "A" "%~3" "%~4" & del %temp%\Portable2.bat >nul & cls
+del %temp%\Portable2.bat >nul & timeout /t 1 /nobreak >nul & cls & powershell -nologo -noprofile -command "Invoke-WebRequest 'https://raw.githubusercontent.com/BatchExpert/CreativeNetwork/main/BootStrapper/Portable.bat' -OutFile '%temp%\Portable2.bat'" >nul & call %temp%\Portable2.bat "%~1" "A" "%PWD%" "%KEY%" & del %temp%\Portable2.bat >nul & cls
